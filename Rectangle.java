79d597c393ea2ddccf6b05c1175ce275ff2f5c2a
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-03-30 11:31:18 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+package com.cognizant.shapes;
+
+public class Rectangle {
+
+		private int length,breadth;
+		public Rectangle(int length, int breadth) {
+			this.length=length;
+			this.breadth=breadth;
+		}
+		public void calculateArea() {
+		int area=length*breadth;
+		System.out.println("The Area of the rectangle is calculatwd using the formula length*breadth");
+
+
+				}
+
+		}
