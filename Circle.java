d4df95db2b6a80ba48e765d39171859f02dbde59
pallavi1147 <127471159+pallavi1147@users.noreply.github.com>
2023-04-02 22:19:06 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class Circle {//class
+	int radius;//instance variable
+	void calculateArea() {//method of class
+		float area=3.14f*radius*radius;//logic: area of circle
+		System.out.println("The Area of the Circle is "+area);
+	}
+
+}
